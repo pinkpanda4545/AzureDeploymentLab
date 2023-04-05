2e350547efc5fce25938a8fb5b058c65ac19e34d
pinkpanda4545 <pinkpanda4545@gmail.com>
2023-04-04 21:06:40 -0500

    Added comment to make azure commit again

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//Trying to fix the Azure update
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Deployment Lab' });
 });
